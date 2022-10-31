e53baf080a223496c3c1c2ab808d80f963cbcecf
haseebpoolakkal <haseebpoolakkal@gmail.com>
2022-10-31 12:11:36 +0530

    Added animation for Side Nav items

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,41 +1,50 @@
 import React from "react";
 import { Link } from "react-scroll";
-import { motion } from 'framer-motion';
-import { useState } from "react";
+import { motion, useAnimation } from "framer-motion";
 
-const NavItem = ({id, name}) => {
-  const initStyle = {
-    opacity: 0,
-    transform: 'translate(0px, 0px)'
-    
-  };
-  const [pStyle, setPStyle] = useState(initStyle);
+const NavItem = ({ id, name }) => {
+  const animation = useAnimation();
 
   const handleHoverStart = () => {
-    setPStyle({
+    animation.start({
+      x: 15,
       opacity: 1,
-      transform: 'translate(10px, 0px)'
-    })
-  }
+      scale: 1,
+      transition: {
+        type: "spring",
+        duration: 1,
+        bounce: 0.5,
+      },
+    });
+  };
 
   const handleHoverEnd = () => {
-    setPStyle({
+    animation.start({
+      x: -100,
       opacity: 0,
-    transform: 'translate(0px, 0px)'
-    })
-  }
+      scale: 0.8,
+    });
+  };
 
-  return <Link to={id} smooth={true} duration={1000} className="mx-12 my-5">
-    <motion.div 
-     whileHover={{ scale: 1.2 }}
-     whileTap={{ scale: 0.9 }}
-     transition={{ type: "spring", stiffness: 200, damping: 10 }}
-    className="w-2 h-14 bg-line rounded cursor-pointer" onHoverStart={handleHoverStart} onHoverEnd={handleHoverEnd}>
-      <p
-       style={pStyle}
-      className="text-white pt-4 ms-4">{name}</p>
-    </motion.div>
-  </Link>;
+  return (
+    <Link to={id} smooth={true} duration={1000} className="mx-12 my-5 flex">
+      <motion.div
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.9 }}
+        transition={{ type: "spring", stiffness: 200, damping: 10 }}
+        className="w-2 h-14 bg-line rounded cursor-pointer"
+        onHoverStart={handleHoverStart}
+        onHoverEnd={handleHoverEnd}
+      ></motion.div>
+      <motion.p
+        initial={{ opacity: 0 }}
+        animate={animation}
+        className="text-white pt-4 -z-10"
+      >
+        {name}
+      </motion.p>
+    </Link>
+  );
 };
 
 export default NavItem;
